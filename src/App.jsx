5e75524501d09d98578fcf9a6d11f1084970eedb
imaosazee <imaosazee@gmail.com>
2026-09-02 14:13:02 +0100

    Configure Tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <main className="min-h-screen bg-slate-100 p-8">
+      <h1 className="text-3xl font-bold underline">
+        LTC Nigeria App
+      </h1>
+    </main>
+  );
+}
+
+export default App;
